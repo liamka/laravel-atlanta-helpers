96875ff4851f7f632db974346625180523fc5fd9
Liamka <liamka@me.com>
2023-03-02 11:59:21 +0300

    Fixes

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -128,3 +128,58 @@ if (!function_exists('taps')) {
 
     }
 }
+
+if (!\function_exists('e_cli')) {
+    function e_cli($string, $foreground_color = 'green', $background_color = null): void
+    {
+        $foreground_colors = [];
+        $background_colors = [];
+        if (is_cli() === true) {
+            $foreground_color = @strtolower($foreground_color);
+            $background_color = @strtolower($background_color);
+
+            $foreground_colors['black'] = '0;30';
+            $foreground_colors['dark_gray'] = '1;30';
+            $foreground_colors['blue'] = '0;34';
+            $foreground_colors['light_blue'] = '1;34';
+            $foreground_colors['green'] = '0;32';
+            $foreground_colors['light_green'] = '1;32';
+            $foreground_colors['cyan'] = '0;36';
+            $foreground_colors['light_cyan'] = '1;36';
+            $foreground_colors['red'] = '0;31';
+            $foreground_colors['light_red'] = '1;31';
+            $foreground_colors['purple'] = '0;35';
+            $foreground_colors['light_purple'] = '1;35';
+            $foreground_colors['brown'] = '0;33';
+            $foreground_colors['yellow'] = '1;33';
+            $foreground_colors['light_gray'] = '0;37';
+            $foreground_colors['white'] = '1;37';
+
+            $background_colors['black'] = '40';
+            $background_colors['red'] = '41';
+            $background_colors['green'] = '42';
+            $background_colors['yellow'] = '43';
+            $background_colors['blue'] = '44';
+            $background_colors['magenta'] = '45';
+            $background_colors['cyan'] = '46';
+            $background_colors['light_gray'] = '47';
+
+            $colored_string = '';
+
+            // Check if given foreground color found
+            if (isset($foreground_colors[$foreground_color])) {
+                $colored_string .= "\033[" . $foreground_colors[$foreground_color] . 'm';
+            }
+
+            // Check if given background color found
+            if (isset($background_colors[$background_color])) {
+                $colored_string .= "\033[" . $background_colors[$background_color] . 'm';
+            }
+
+            // Add string and end coloring
+            $colored_string .= $string . "\033[0m";
+
+            echo($colored_string . \PHP_EOL);
+        }
+    }
+}
